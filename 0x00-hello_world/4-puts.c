ed8b20d849c9e54524866bbe207e1996e4a179ae
olubisi <ibrowale3gmail.com>
2022-12-09 15:08:01 -0800

    hello world task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  *  * main - Entry
  *   * Return: Always 0
  *   */
@@ -8,4 +8,3 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
-
