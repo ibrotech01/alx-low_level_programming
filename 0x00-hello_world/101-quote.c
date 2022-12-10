eb146a54f386e733cca576dbe35ee1e7668386e4
olubisi <ibrowale3gmail.com>
2022-12-09 19:25:02 -0800

    hello world unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry
- * Return: Always 0 (success)
+ * Return: 1
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 			return (1);
 }
