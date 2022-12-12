a333b57dec9b403ee584536dc5dce106ddfe3fce
olubisi <ibrowale3gmail.com>
2022-12-12 11:12:08 -0800

    6 size

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry
  *
  * Return: Always 1 (success)
  */
-
 int main(void)
 {
-	write(1, "and that piece of art is useful\n");
+	printf(1, "and that piece of art is useful\n");
 	return (1);
 }
