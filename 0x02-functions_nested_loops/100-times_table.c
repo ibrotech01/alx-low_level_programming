4088e7e0cbfbfa93509103cf4cb87a7d60541d70
olubisi <ibrowale3gmail.com>
2022-12-15 14:16:23 -0800

    time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,6 +30,12 @@ void print_times_table(int n)
 				_putchar(' ');
 				if (product <= 9)
 				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + product);
+				}
+				else if (product > 9 && product < 100)
+				{
 					_putchar(',');
 					_putchar('0' + (product / 10));
 					_putchar('0' + (product % 10));
