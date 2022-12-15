0c2a9c684fc61785e9fb80eb8b77571e92a417a0
olubisi <ibrowale3gmail.com>
2022-12-15 15:19:33 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ * Return: something
+ */
+
+int main(void)
+{
+	long int i, j, k, l;
+
+	j = 1;
+	k = 2;
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20365011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		i = j + k;
+		j = k;
+		k = i;
+	}
+	return (0);
+}
