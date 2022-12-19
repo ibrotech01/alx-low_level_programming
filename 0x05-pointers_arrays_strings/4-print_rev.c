ad7db085eb02cb8ad13c0f715d614a608efd44ce
olubisi <ibrowale3gmail.com>
2022-12-19 08:51:00 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,7 +15,7 @@ void print_rev(char *s)
 	while (*s != '\0')
 	{
 		longi++;
-		s++
+		s++;
 	}
 	s--;
 	for (o = longi; o > 0; o--)
@@ -24,4 +24,3 @@ void print_rev(char *s)
 		s--;
 	}
 	_putchar('\n');
-}
