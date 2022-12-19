baa22d3a549ce168af85945f1b92271ebebe909c
olubisi <ibrowale3gmail.com>
2022-12-19 10:33:11 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,17 +13,11 @@ void puts_half(char *str)
 
 	longi = 0;
 	for (a = 0; str[a] != '\0'; a++)
-	{
 		longi++;
 		n = (longi / 2);
-	}
 	if ((longi % 2) == 1)
-	{
 		n = ((longi + 1) / 2);
-	}
 	for (a = n; str[a] != '\0'; a++)
-	{
 		_putchar(str[a]);
-	}
 	_putchar('\n');
 }
