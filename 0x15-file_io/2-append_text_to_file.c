dc4a8d0498d37893cc375760cda3a2be9eb37128
ibrotech01 <ibrowale3@gmail.com>
2023-02-06 16:17:42 -0800

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			i++;
 		chk = write(fild, text_content, i);
 		if (chk < 0)
-				return (-1);
+			return (-1);
 	}
 	close(fild);
 	return (1);
