ca70048bb168d392823623eb21ae5d2e133cc324
ibrotech01 <ibrowale3@gmail.com>
2023-03-20 15:13:57 -0700

    task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "ctype.h"
+
+/**
+ * islower - checks if a character is lowercase if YES it returns 1
+ * otherwise returns 0 just like islower() function in ctype.h
+ * @c: Is the int value to be compared with the ASCII value
+ * Return: 0 if successful. _islower - Entry point
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
